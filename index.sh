d524ca13a9f0f3f93fc263793c870e9c673f4909
CamiloTDex <camilotd1999@gmail.com>
2024-04-29 16:49:09 -0500

    Refactor index.sh to display free disk space and remove commented lines

--- index.sh
+++ index.sh
@@ -24,7 +24,7 @@ display_free_memory () {
 }
 
 display_free_disk () {
-    local free_disk=$(df -h | grep '/$' | awk '{ print $5 }')
+    local free_disk=$(df -h | grep '/$' | awk '{ print $5 }' | awk -F'%' '{ printf "%d%%\n", 100 - $1 }')
 
     print "  \x1b[1mFree Disk:\x1b[0m" $free_disk
 }
@@ -33,6 +33,6 @@ display_free_cpu
 display_free_memory
 display_free_disk
 
-print ""
-print "  \x1b[36m?\x1b[0m To view the current services status, use the \`\x1b[33mvm services\x1b[0m\` command"
-print "  \x1b[36m?\x1b[0m To view the system status, use the \`\x1b[33mvm status\x1b[0m\` command"
+# print ""
+# print "  \x1b[36m?\x1b[0m To view the current services status, use the \`\x1b[33mvm services\x1b[0m\` command"
+# print "  \x1b[36m?\x1b[0m To view the system status, use the \`\x1b[33mvm status\x1b[0m\` command"
