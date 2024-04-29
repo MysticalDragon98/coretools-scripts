aeb6576b5dc850d682b64cb339f3dfe87a0fc11c
CamiloTDex <camilotd1999@gmail.com>
2024-04-29 16:19:57 -0500

    Refactor index.sh to display CPU usage and add commands for viewing services and system status

--- index.sh
+++ index.sh
@@ -3,3 +3,13 @@ clear
 if [ -f ~/welcome.txt ]; then
     cat welcome.txt | while IFS= read -r line; do echo -e "$line"; done
 fi
+
+# Display CPU Usage
+display_free_cpu () {
+    local free_cpu=$(mpstat -o JSON | jq '.sysstat.hosts[0].statistics[0]["cpu-load"][0].idle')
+
+    echo "\x1b[1mFree CPU:\x1b[0m" $free_cpu%
+}
+
+echo "  \x1b[36m?\x1b[0m To view the current services status, use the `\x1b[33mvm services\x1b[0m` command"
+echo "  \x1b[36m?\x1b[0m To view the system status, use the `\x1b[33mvm status\x1b[0m` command"
