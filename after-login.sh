88028021098edc820a3d94b9b351ec83a51abf7d
CamiloTDex <camilotd1999@gmail.com>
2024-04-29 12:46:38 -0500

    Refactor after-login.sh to include a login function

--- after-login.sh
+++ after-login.sh
@@ -1,1 +1,12 @@
-sudo su admin
+login () {
+    local user=$1
+    local current_user=$(whoami)
+
+    if "$current_user" -eq "root" ; then
+        return
+    fi
+
+    if [ "$current_user" != "$user" ]; then
+        sudo su $user
+    fi
+}
